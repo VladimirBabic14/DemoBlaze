2dcff00c126addf7e13ec7281dff5c9fa330535c
Vladimir <vladimirbabic14@gmail.com>
2023-07-12 09:55:16 +0200

    AddToCart

---
+++ src/main/java/NaviPage.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class NaviPage extends BasePage
+{
+    @FindBy(linkText = "Home ")
+    WebElement home;
+
+    @FindBy(linkText = "Contact")
+    WebElement contact;
+
+    @FindBy(linkText = "About us")
+    WebElement aboutUs;
+
+    @FindBy(id = "cartur")
+    WebElement cart;
+
+    public NaviPage(ChromeDriver driver)
+    {
+    this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+}
